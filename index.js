9f5660dbdabcb2fb148badd68d1b5c041b31d2a8
Hernan Eladio Encizo <hernanencizo360@gmail.com>
2024-03-11 10:44:42 -0300

    se agrego en el .js las funciones de captura de datos

--- index.js
+++ index.js
@@ -2,32 +2,33 @@ let id_article_encriptar = "article_encriptar",
   id_article_desencriptar = "article_desencriptar",
   id_article_copiar = "article_copiar";
 
+/********************************************
+ * Funciones para mostrar la seccion oculta (sección encriptar) del HTML -> Éstas f(x) estan asociadas a los btns de la sección Home.
+ ****************************************** */
 function mostrarArticleEncriptar() {
-  let articleEncriptar = document.getElementById(id_article_encriptar);
-  let articleCopiar = document.getElementById("article_copiar");
+  let articleEncriptar = document.getElementById("article_encriptar");
 
-  //Antes de activar el display llamo al ocultarContenido para ocultar el article_desencriptar si es que esta visible;
-  ocultarContenido(id_article_desencriptar);
+  //Antes de activar el display llamo al ocultarContenido para ocultar el articulo desencriptar si es que esta visible;
+  ocultarContenido("article_desencriptar");
 
   //remuevo la clase oculta para mostrar en pantalla;
   articleEncriptar.classList.remove("oculta");
-  articleCopiar.classList.remove("oculta");
+
+  mostrarArticleCopiar();
   dirigirASeccion();
 }
 
 function mostrarArticleDesencriptar() {
   let article = document.getElementById(id_article_desencriptar);
-  let articleCopiar = document.getElementById("article_copiar");
 
-  //Antes de activar el display llamo al ocultarContenido para ocultar el article_desencriptar si es que esta visible;
   ocultarContenido(id_article_encriptar);
-
-  //remuevo la clase oculta para mostrar en pantalla;
   article.classList.remove("oculta");
-  articleCopiar.classList.remove("oculta");
+
+  mostrarArticleCopiar();
   dirigirASeccion();
 }
 
+// F(x) para ocultar contenido si tiene no tiene la clase oculta(display:none) se la agrega.
 function ocultarContenido(id) {
   let articulo = document.getElementById(id);
   if (!articulo.classList.contains("oculta")) {
@@ -35,24 +36,36 @@ function ocultarContenido(id) {
   }
 }
 
+function mostrarArticleCopiar() {
+  let articleCopiar = document.getElementById("article_copiar");
+  articleCopiar.classList.remove("oculta");
+}
+
 function dirigirASeccion() {
   let irASeccion = document.getElementById("seccion__encriptar");
+
   irASeccion.scrollIntoView({ behavior: "smooth" });
 }
 
+/*********************************************
+ * MOSTRADAS LAS SECCIONES EMPEZAMOS A CAPTURAR LOS DATOS INGRESADOS POR EL USUARIO
+ ********************************************/
+
 /*
 Las "llaves" de encriptación que utilizaremos son las siguientes:
-
 La letra "a" es convertida para "ai"
 La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
 La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat" */
+La letra "u" es convertida para "ufat" 
+*/
+
+//DECLARACIÓN DE VARIABLES Y CONSTANTES:
 const llaves = ["ai", "enter", "imes", "ober", "ufat"];
 
 let texto_a_encriptar, texto_a_desencriptar;
 
-//FUNCION ENCRIPTAR
+//f(x) PROPIAMENTE DICHA DE ENCRIPTAR
 function encriptar() {
   try {
     //capturo el texto
@@ -65,17 +78,37 @@ function encriptar() {
     console.log(`El texto a encriptar es: ${texto_a_encriptar}`);
   } catch (error) {
     alert(error.message);
-    //Limpiar el texarea;
-    limpiarTextarea();
+    limpiarTextarea("texto_a_encriptar");
   }
 }
 
-//CAPTURAR LOS DATOS DEL USUARIO:
+//f(x) PROPIAMENTE DICHA DE DESENCRIPTAR
+function desencriptar() {
+  try {
+    //capturo el texto
+    capturarTextoADesencriptar();
+    //validar el texto.
+    validarTexto(texto_a_desencriptar);
+
+    //Si es valido remplazar los valores;
+    /* Por ahor lo mostramos solo por consola */
+    console.log(`El texto a desencriptar es: ${texto_a_desencriptar}`);
+  } catch (error) {
+    alert(error.message);
+    limpiarTextarea("texto_a_desencriptar");
+  }
+}
+
+//capturamos los datos y lo almacenamos en la variable texto_a_encriptar; previamente definida :
 function capturarTextoAEncriptar() {
   texto_a_encriptar = document.getElementById("texto_a_encriptar").value;
 }
 
-/* Función principal para validar el texto */
+function capturarTextoADesencriptar() {
+  texto_a_desencriptar = document.getElementById("texto_a_desencriptar").value;
+}
+
+/* f(x) para validar el texto: */
 function validarTexto(texto) {
   try {
     //LLamada a las demas funciones
@@ -84,57 +117,43 @@ function validarTexto(texto) {
     contieneLetrasConAcento(texto);
     contieneCaracteresEspeciales(texto);
 
-    //Si pasa todas simplemente es valido
-    console.log("El texto es válido.");
+    //Si pasa todas simplemente es valido y continuamos con la secuencia
   } catch (error) {
     throw new Error(error);
   }
 }
 
-// Función para validar si el texto contiene mayúsculas
+// f(x) para validar si el texto contiene mayúsculas - si contiene lanzamos un error.
 function contieneMayusculas(texto) {
   if (/[A-Z]/.test(texto)) {
     throw new Error("El texto contiene mayúsculas.");
   }
 }
 
-// Función para validar si el texto contiene números
+// f(x) para validar si el texto contiene números
 function contieneNumeros(texto) {
   if (/\d/.test(texto)) {
     throw new Error("El texto contiene números.");
   }
 }
 
-// Función para validar si el texto contiene letras con acentos
+// f(x) para validar si el texto contiene letras con acentos
 function contieneLetrasConAcento(texto) {
   if (/[áéíóúÁÉÍÓÚ]/.test(texto)) {
     throw new Error("El texto contiene letras con acento.");
   }
 }
 
-// Función para validar si el texto contiene caracteres especiales
-//En este caso admito los signos de punto,coma y ñ pero los podemos modificar
+// f(x) para validar si el texto contiene caracteres especiales
+//En este caso escapamos(admitimos) los signos de punto,coma y ñ.
 function contieneCaracteresEspeciales(texto) {
   if (/[^\w\s\.\,\ñ]/.test(texto)) {
     throw new Error("El texto contiene caracteres especiales.");
   }
 }
 
-//Limpiar textarea
-function limpiarTextarea() {
-  let textarea = document.getElementById("texto_a_encriptar");
+//f(x) para limpiar textarea
+function limpiarTextarea(id) {
+  let textarea = document.getElementById(id);
   textarea.value = "";
-}
-
-//FUNCION ENCRIPTAR EL MENSAJE
-
-/*>>>>>>>>>>>>>>DESENCRIPTAR<<<<<<<<<<<*/
-function capturarTextoADesencriptar() {
-  let texto = document.getElementById("texto_a_desencriptar").value;
-  /*  try {
-    validarTexto(texto);
-    console.log(texto);
-  } catch (error) {
-    alert(error.message);
-  } */
 }
